155c3023eff364101e152b212efe29ed66704082
faisalfarizi22 <faisal.alfariziak@gmail.com>
2025-10-10 06:54:36 +0700

    test push

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    turbo: false,
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
